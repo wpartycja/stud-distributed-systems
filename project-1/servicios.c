6f6b2194cf6fc5c05ac176da4ebcdde04e065714
Patrycja Wysocka <w512patrycja@gmail.com>
2023-03-15 13:54:55 +0100

    finished modify_value function

--- project-1/servicios.c
+++ project-1/servicios.c
@@ -3,13 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <math.h>
+
 #include "servicios.h"
+
 #define MAX_MSG_SIZE 1024
+#define DIR_NAME "FilesPractice1"
+#define FILE_TYPE ".txt"
+
 
 int init() {
-	DIR* dir = opendir("FilesPractice1");
+	DIR* dir = opendir(DIR_NAME);
 	struct dirent *nextFile;
 	char filePath[1024];
 
@@ -17,7 +27,7 @@ int init() {
 	if(dir) {
 		// If directory exists, we proceed to delete all the files within it.
 		while((nextFile = readdir(dir)) != NULL){
-			sprintf(filePath, "%s/%s", "FilesPractice1", nextFile->d_name);
+			sprintf(filePath, "%s/%s", DIR_NAME, nextFile->d_name);
 			remove(filePath);
 		}
 		
@@ -29,7 +39,7 @@ int init() {
 
 	} else if (ENOENT == errno){
 		// If the directory doesnt exist, we create it. 
-		mkdir("FilesPractice1", 0700);
+		mkdir(DIR_NAME, 0700);
 	} else {
 		perror("Error while opening the directory.\n");
 		exit(-1);
@@ -48,9 +58,40 @@ int init() {
 
 // }
 
-// int modify_value(int key, char, *value1, int value2, double value3){
+int modify_value(int key, char* value1, int value2, double value3){
+	// initializing variables
+	// + adding variables of type char[] for values
+	FILE* fptr;
 
-// }
+	char key_str[(int)((ceil(log10(key))+1)*sizeof(char))]; // inside fancy way to calculate dynamically size of 
+	char value2_str[(int)((ceil(log10(value2))+1)*sizeof(char))];
+	char value3_str[(int)((ceil(log10(value3))+1)*sizeof(char))];
+
+	sprintf(key_str, "%d", key);
+	sprintf(value2_str, "%d", value2);
+	sprintf(value3_str, "%f", value3);
+
+    // creating a path to file
+	char path[strlen(DIR_NAME) + strlen(key_str) + strlen(FILE_TYPE) + 2]; // 2 becuase of "/" and EOF
+	snprintf(path, sizeof(path), "%s/%s%s", DIR_NAME, key_str, FILE_TYPE);
+
+	// creating new file content
+	int size = strlen(key_str) + strlen(value1) + strlen(value2_str) + strlen(value3_str) + 3; // "3" for separating commas
+	char new_line[size];
+	snprintf(new_line, sizeof(new_line), "%s,%s,%s,%s", key_str, value1, value2_str, value3_str);
+
+	//saving to file
+	fptr = fopen(path, "r+");
+	 
+    if (NULL == fptr) {
+        printf("file can't be opened \n");
+    }
+
+	fprintf(fptr, "%s", new_line);
+    fclose(fptr);
+
+    return 0;
+}
 
 // int delete_key(int key){
 
