f8861924a01df17f134878f430d4813734ddcdee
Patrycja Wysocka <w512patrycja@gmail.com>
2023-03-15 16:30:34 +0100

    added working exist function!

--- project-1/servicios.c
+++ project-1/servicios.c
@@ -114,6 +114,30 @@ int delete_key(int key){
 	return 0;
 }
 
+int exist(int key){
+	// add variable key in char[] type
+	int key_mem;
+	if (key < 0){
+		key_mem = -key;
+	} else if (key == 0){
+		key_mem = 1;
+	} else {
+		key_mem = key;
+	}
+	char key_str[(int)((ceil(log10(key_mem))+1)*sizeof(char))]; // inside fancy way to calculate dynamically size of key_str
+	sprintf(key_str, "%d", key);
+
+	// creating a path to file
+	char path[strlen(DIR_NAME) + strlen(key_str) + strlen(FILE_TYPE) + 2]; // 2 becuase of "/" and EOF
+	snprintf(path, sizeof(path), "%s/%s%s", DIR_NAME, key_str, FILE_TYPE);
+
+	if (access(path, F_OK) == 0) { // F_OK - test for the existence of the file
+		return 1; // file exist
+	} else {
+		return 0; // file doesn't exist
+	}
+}
+
 // int copy_key(int key1, int key2){
 
 // }
