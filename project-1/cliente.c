bc4fd475bbf288d7af321c93f419c2e3088d71ac
IlseCS <47673144+IlseCS@users.noreply.github.com>
2023-03-17 00:08:02 +0100

    Update cliente.c

--- project-1/cliente.c
+++ project-1/cliente.c
@@ -1,7 +1,18 @@
 #include "clave.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    exist(2);
+    exist(1);
+    copy_key(1, 3);
+    set_value(2, "lalal", 5, 6);
+    
+
+    char val1[256];
+    int *val2 = malloc(sizeof(int));  
+    double *val3 = malloc(sizeof(double));  
+
+    get_value(1, val1, val2, val3);
+
     return 0;
     }
