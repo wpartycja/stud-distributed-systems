7658669f5bcfb3fb9f3428549f2fa71764b4191a
IlseCS <47673144+IlseCS@users.noreply.github.com>
2023-03-17 00:10:14 +0100

    Update servicios.h

--- project-1/servicios.h
+++ project-1/servicios.h
@@ -12,9 +12,9 @@ struct tuple {
 
 int init();
 
-// int set_value(int key, char *value1, int value2, double value3);
+int set_value(int key, char *value1, int value2, double value3);
 
-// int get_value(int key, char *value1, int *value2, double *value3);
+int get_value(int key, char *value1, int *value2, double *value3);
 
 int modify_value(int key, char *value1, int value2, double value3);
 
@@ -22,8 +22,6 @@ int delete_key(int key);
 
 int exist(int key);
 
-// int copy_key(int key1, int key2);
+int copy_key(int key1, int key2);
 
 #endif
-
-
