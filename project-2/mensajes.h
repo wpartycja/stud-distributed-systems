43159d70cc812e0163f87994d609be725aafd94f
Patrycja Wysocka <w512patrycja@gmail.com>
2023-03-23 12:36:34 +0100

    project2: added base files from pro1

---
+++ project-2/mensajes.h
@@ -0,0 +1,23 @@
+#ifndef _MENSAJES_H
+#define _MENSAJES_H 
+
+#define MAXSIZE 256
+struct response {
+    int result; 
+    char value1[MAXSIZE];
+    int value2;
+    double value3;
+};
+
+
+struct request {
+    int operation_id;
+    char queue_name[MAXSIZE]; 
+    int key;
+    char value1[MAXSIZE];
+    int value2; // can be used as key2 in copy_key() func
+    double value3; 
+
+};
+
+#endif
